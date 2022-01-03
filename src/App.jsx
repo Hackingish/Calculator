7d971c4e6411103644e6229d4bfc3435d17d129b
Hackingish <subhasispanda876@gmail.com>
2022-01-03 10:00:00 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,61 @@
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react";
+
+
+const App = () => {
+  
+  const [input,setInput] = useState("");
+
+  const handleClick = (value) =>{
+    setInput((prev)=> prev + value);
+  }
+
+  const handleClear = () =>{
+    setInput("");
+  }
+
+  const handleDelete = () => {
+    setInput((prev) => prev.slice(0,-1));
+  }
+
+  const handleCalc = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput("Error");
+    }
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="calc-container">
+      <div className="calc">
+        <div className="display">{input || "0"}</div>
+        <div className="buttons">
+          <button className="btn control" onClick={handleClear}>AC</button>
+          <button className="btn control" onClick={handleDelete}>DEL</button>
+          <button className="btn operator" onClick={() => handleClick("%")}>%</button>
+          <button className="btn operator" onClick={() => handleClick("/")}>/</button>
 
-      <div className="ticks"></div>
+          <button className="btn" onClick={() => handleClick("7")}>7</button>
+          <button className="btn" onClick={() => handleClick("8")}>8</button>
+          <button className="btn" onClick={() => handleClick("9")}>9</button>
+          <button className="btn operator" onClick={() => handleClick("*")}>X</button>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+          <button className="btn" onClick={() => handleClick("4")}>4</button>
+          <button className="btn" onClick={() => handleClick("5")}>5</button>
+          <button className="btn" onClick={() => handleClick("6")}>6</button>
+          <button className="btn operator" onClick={() => handleClick("-")}>-</button>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+          <button className="btn" onClick={() => handleClick("1")}>1</button>
+          <button className="btn" onClick={() => handleClick("2")}>2</button>
+          <button className="btn" onClick={() => handleClick("3")}>3</button>
+          <button className="btn operator" onClick={() => handleClick("+")}>+</button>
+
+          <button className="btn zero" onClick={() => handleClick("0")}>0</button>
+          <button className="btn" onClick={() => handleClick(".")}>.</button>
+          <button className="btn equals" onClick={handleCalc}>=</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
